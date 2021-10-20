c549cfbc951281a77933895223f0c60f0a0e8538
James Jarvis <git@jamesjarvis.io>
2021-10-20 22:21:15 +0100

    doesnt do much yet, but has a hello world thing

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/jamesjarvis/js-in-go
 
 go 1.17
+
+require rogchap.com/v8go v0.6.0
--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"rogchap.com/v8go"
+)
+
+// exampleScript is a basic script that presents a function being called with an object
+// that has an accessor function attached to it.
+// This also presents use of a global "jarvis" object with callback functions.
+const exampleScript = `
+function thing(request) {
+	req_id = request.get_request_id()
+  jarvis.add_note(req_id)
+}
+`
 
 func main() {
-	fmt.Println("hello world!")
+	ctx, err := v8go.NewContext(nil) // creates a new V8 context with a new Isolate aka VM
+	if err != nil {
+		panic(err)
+	}
+	ctx.RunScript("const add = (a, b) => a + b", "math.js") // executes a script on the global context
+	ctx.RunScript("const result = add(3, 4)", "main.js") // any functions previously added to the context can be called
+	val, err := ctx.RunScript("result", "value.js") // return a value in JavaScript back to Go
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("addition result: %s\n", val)
 }
